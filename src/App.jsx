07599ff6b4ac9f26a394c802d899e2e1d0b5d99b
Oleksii Verbytskyi <alverba88@gmail.com>
2022-06-22 19:44:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+
+import { Container } from './components/Container/Container';
+
+export const App = () => {
+  return (
+    <Container>
+      <div>hello</div>
+    </Container>
+  );
+};
