987ad64666d6bc00df6d6c597988562bc16184f0
Oleksii Verbytskyi <alverba88@gmail.com>
2022-06-25 15:50:06 +0300

    added logOut form

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,11 +1,13 @@
 import styles from './AppBar.module.css';
 import { Navigation } from '../Navigation';
 import { AuthNav } from '../AuthNav';
+import { UserMenu } from '../UserMenu';
 
 export const AppBar = () => {
   return (
     <header className={styles.header}>
       <Navigation />
+      <UserMenu />
       <AuthNav />
     </header>
   );
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,5 +1,16 @@
+import * as React from 'react';
+
+import Button from 'react-bootstrap/Button';
 import styles from './UserMenu.module.css';
 
 export const UserMenu = () => {
-  return <div>UserMenu</div>;
+  return (
+    <div style={styles.container}>
+      <img alt="" width="32" style={styles.avatar} />
+      <span style={styles.name}>Добро пожаловать, User</span>
+      <Button variant="outline-primary" className={styles.logOut}>
+        logout
+      </Button>
+    </div>
+  );
 };
