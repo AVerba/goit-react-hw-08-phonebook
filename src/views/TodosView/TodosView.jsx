a7385c8f91b4b87b4ac2c57bacf54e30f4d4d614
Oleksii Verbytskyi <alverba88@gmail.com>
2022-06-30 19:23:58 +0300

    fix mistakes at todos list

--- src/views/TodosView/TodosView.jsx
+++ src/views/TodosView/TodosView.jsx
@@ -1,5 +1,13 @@
 import styles from './TodosView.module.css';
+import { Container } from '@mui/material';
+import { Title } from '@mui/icons-material';
 
 export const TodosView = () => {
-  return <div>TodosView</div>;
+  return (
+    <Container>
+      <h4 className={styles.title}>
+        We are sorry, but page at the moment under construction
+      </h4>
+    </Container>
+  );
 };
